cb81ebf1a106d8873a43962acabf7d19bd208796
Arianna Brisco <121651388+ariannabrisco@users.noreply.github.com>
2024-04-13 01:31:17 -0500

    Rilla & bit of org

--- Arianna.py
+++ Arianna.py
@@ -3,6 +3,10 @@ Name: Rilla
 Description: A peppy gorilla based on the Hello Kitty franchise
 Sanrio Character: Hello Kitty
 Personality: Peppy
+Personality Description: Peppy villagers are energetic, friendly, and overly excited, allowing them to get along easily
+with many villagers. In New Horizons, peppy villagers will also sometimes say "très" (French for "very") in their
+dialogue, and tend to be excited on days when K.K. Slider appears on the island. They also love to talk about becoming
+a recording star, saying phrases like "Want to practice being pop stars together?".
 Species: Gorilla
 Gender: Female
 Birthday: November 1
@@ -10,4 +14,24 @@ Catchphrase: "hello"
 Hobbies: Nature
 Activities: May be found reading a book pertaining to an item she is studying, which can be either flowers, bugs,
 or fossils.
+Favorite Styles: Cute, Elegant
+Favorite Colors: Red, Blue
+Default Clothing: Hello Kitty Tee
+Music: Bubblegum K.K.
+Furniture:
+- Hello Kitty bed
+- Hello Kitty chair
+- Hello Kitty clock
+- Hello Kitty drawers
+- Hello Kitty table
+- Hello Kitty planter
+- Hello Kitty poster
+- Hello Kitty wall
+- Hello Kitty flooring
+- Hello Kitty rug
+Clothing:
+- Hello Kitty dress
+- Hello Kitty shoes
+- Hello Kitty hat
+- Hello Kitty tee
 """
